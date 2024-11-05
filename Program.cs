a72f917d6a60b18d8b782674ffac3d94f5dec299
franco riffo <franco.riffoa@correoaiep.cl>
2024-11-04 21:48:00 -0300

    primer commit proyecto listo

---
+++ Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+
+namespace Calculadora
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Seleccione una operación:");
+            Console.WriteLine("1. Sumar");
+            Console.WriteLine("2. Restar");
+
+
+            int opcion = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el primer número:");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+
+            Console.WriteLine("Ingrese el segundo número:");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+
+            double resultado = 0;
+
+            switch (opcion)
+            {
+                case 1:
+                    resultado = Sumar(num1, num2);
+                    break;
+                case 2:
+                    resultado = Restar(num1, num2);
+                    break;
+                case 3:
+              
+                default:
+                    Console.WriteLine("Opción no válida");
+                    break;
+            }
+
+            Console.WriteLine("El resultado es: " + resultado);
+        }
+
+        static double Sumar(double a, double b)
+        {
+            return a + b;
+        }
+
+        static double Restar(double a, double b)
+        {
+            return a - b;
+        }
+
+        static double Multiplicar(double a, double b)
+        {
+            return a * b;
+        }
+
+        static double Dividir(double a, double b)
+        {
+            if (b != 0)
+            {
+                return a / b;
+            }
+            else
+            {
+                Console.WriteLine("Error: División por cero");
+                return 0;
+            }
+        }
+    }
+}
+
